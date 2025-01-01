d20b72a758e16b6097c3e42ac3894311fe096980
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-01 15:52:27 -0600

    reimplemented to dos and currently working on

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -12,10 +12,18 @@ function Homepage() {
       {/* <Experience /> */}
       <ProjectsPreview />
       <Technologies />
-      {/* <div>
+      <div>
         <h2>Here&#39;s what I&#39;m Currently Working On</h2>
+        <h2>Overarching Goal</h2>
+        <p>Improve the quality of the projects on this site.</p>
         <h2>Current To Do&#39;s</h2>
-      </div> */}
+        <p>Learn 2 more core languages. probably python and C#</p>
+        <p>Make all Projects Full stack.</p>
+        <p>
+          Complete some better projects and take off the simple boring ones.
+        </p>
+        <p>Add photos of projects.</p>
+      </div>
     </div>
   );
 }
