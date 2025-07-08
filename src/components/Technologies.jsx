b0349f881821c8c1edc643af96535eb9d6ad9be8
CalvinGaiennie <calvingaiennie@gmail.com>
2025-07-07 21:10:23 -0500

    added tech

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,7 +8,7 @@ function Technologies() {
         <h1 className={styles.technologiesHeaderTitle}>Technologies</h1>
         <div className={styles.mainTech}>
           {/* <h3>Main Tech</h3> */}
-          <div className={styles.techTable}>
+          {/* <div className={styles.techTable}>
             <div className={styles.techRow}>
               <TechnologyCard name="React" imgSRC="/imgs/react.png" />
               <TechnologyCard name="JavaScript" imgSRC="/imgs/js.webp" />
@@ -20,27 +20,27 @@ function Technologies() {
               <TechnologyCard name="MongoDB" imgSRC="/imgs/mongodb.png" />
             </div>
             <div className={styles.techRow}></div>
-          </div>
-          {/* <div className={styles.techTable}>
+          </div> */}
+          <div className={styles.techTable}>
             <div className={styles.techRow}>
               <TechnologyCard name="React" imgSRC="/imgs/react.png" />
               <TechnologyCard name="JavaScript" imgSRC="/imgs/js.webp" />
               <TechnologyCard name="HTML" imgSRC="/imgs/html.webp" />
               <TechnologyCard name="CSS" imgSRC="/imgs/css.png" />
-              <div className={styles.techRow}>
-                <TechnologyCard name="C#" imgSRC="/imgs/CSHARPLOGO.png" />
-                <TechnologyCard name="MS SQL" imgSRC="/imgs/SQLLogo.png" />
-                <TechnologyCard
-                  name="Angular"
-                  imgSRC="/imgs/angular_gradient.png"
-                />
-                <TechnologyCard name="NodeJS" imgSRC="/imgs/nodejs.webp" />
-              </div>
             </div>
             <div className={styles.techRow}>
+              <TechnologyCard name="MS SQL" imgSRC="/imgs/SQLLogo.png" />
+              <TechnologyCard
+                name="Angular"
+                imgSRC="/imgs/angular_gradient.png"
+              />
               <TechnologyCard name="MongoDB" imgSRC="/imgs/mongodb.png" />
+              <TechnologyCard name="NodeJS" imgSRC="/imgs/nodejs.webp" />
             </div>
-          </div> */}
+            <div className={styles.techRow}>
+              <TechnologyCard name="C#" imgSRC="/imgs/CSHARPLOGO.png" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
